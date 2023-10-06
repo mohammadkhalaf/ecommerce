f0838edb3fdb204f1b3528d4f81676d7e5e66ec1
mohammad <mohammadkhalaf@live.com>
2023-10-06 13:37:48 +0200

    add routes and middleware

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './data/db.js';
-import products from './data/products.js';
+import productRouter from './routes/productsRoutes.js'
 const port=process.env.PORT||5000
 connectDB()
 const app= express();
 app.get('/',(req,res)=>{
-    res.send('sdfsdf')
+    res.status(200).json({msg:"hi"})
 })
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
-app.get('/api/products/:id',(req,res)=>{
-    const product=products.find((p)=>p._id===req.params.id)
-    res.json(product)
-})
+app.use('/api/products',productRouter)
+
 app.listen(port,()=>{
 console.log('sever is running'+ port);
 })
