80a1b4011f870a58e06c83c9132798a6edeaa919
mohammad <mohammadkhalaf@live.com>
2023-10-10 12:13:40 +0200

    integrate with paypal payment

--- frontend/src/statemanagement/slices/orderApiSlice.js
+++ frontend/src/statemanagement/slices/orderApiSlice.js
@@ -16,18 +16,31 @@ export const orderApiSlice=apiSlice.injectEndpoints({
             keepUnusedDataFor:5       
         }),
         payOrder:builder.mutation({
-            query:(orderId,details)=>({
+            query:({orderId,details})=>({
                 url:`${ORDERS_URL}/${orderId}/pay`,
                 method:'PUT',
                 body:{...details}
             }), 
         }),
-        getPayPalClintId:builder.query({
+        getPaypalClientId:builder.query({
             query:()=>({
                 url:PAYPAL_URL
             }),
             keepUnusedDataFor:5 
+        }),
+        deliverOrder:builder.mutation({
+             query: (orderId) => ({
+           url: `${ORDERS_URL}/${orderId}/deliver`,
+           method: 'PUT',
+      }),
         })
+        
     })
 })
-export const {useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPayPalClintIdQuery}=orderApiSlice
+export const { useCreateOrderMutation,
+  useGetOrderDetailsQuery,
+  usePayOrderMutation,
+  useGetPaypalClientIdQuery,
+  useDeliverOrderMutation
+
+}=orderApiSlice
